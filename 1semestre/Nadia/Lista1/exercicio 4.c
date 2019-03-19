1e7f32118cfa36aaca91cc127028f82e49bc8d3d
Cristian Magalhães <cristian123105@gmail.com>
2019-03-19 19:34:11 -0300

    Update exercicio 4.c

--- 1semestre/Nadia/Lista1/exercicio 4.c
+++ 1semestre/Nadia/Lista1/exercicio 4.c
@@ -4,14 +4,14 @@ int main()
 {
     float n1, n2, n3, n4, media, tot;
     printf("Digite a primeira nota: ");
-    scanf("%f", n1);
+    scanf("%f", &n1);
     printf("Digite a segunda nota: ");
-    scanf("%f", n2);
+    scanf("%f", &n2);
     printf("Digite a terceira nota: ");
-    scanf("%f", n3);
+    scanf("%f", &n3);
     printf("Digite a quarta nota: ");
-    scanf("%f", n4);
+    scanf("%f", &n4);
     tot = n1+n2+n3+n4;
     media = tot/5;
-
+    printf("A media e: %f",media);
 }
