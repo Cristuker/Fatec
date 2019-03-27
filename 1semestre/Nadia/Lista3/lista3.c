e2477b1a5a1b8be51072df35cdc541b7fc4a5db7
Crituker <cristian123105@gmail.com>
2019-03-26 23:09:36 -0300

    att da lista 3

--- 1semestre/Nadia/Lista3/lista3.c
+++ 1semestre/Nadia/Lista3/lista3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define ex5
+#define ex7
 
 #ifdef ex1
 int main(){
@@ -44,7 +44,7 @@ int main(){
 }
 #endif // ex2
 //----------------------------------------------------------------
-#ifdef ex3
+#ifdef ex4
  int main(){
 
 int alunos =0,indice=0,nota6=0;
@@ -65,13 +65,13 @@ scanf("%d", &alunos);
      tot= notas/alunos;
      printf("A media da sala e: %.2f", tot );
      printf("\n");
-     printf("%d alunos tiraram nota mais ou igual a 6",nota6);
+     printf("%d alunos tiraram nota maior ou igual a 6",nota6);
 
 
  }
 #endif // ex3
 //----------------------------------------------------------------
-#ifdef ex4
+#ifdef ex3
 int main(){
 
 int num=0,indice=0;
@@ -80,7 +80,7 @@ printf("Digite um numero: ");
 scanf("%d",&num);
 
     while(indice<num){
-        if(indice%2==1){
+        if(indice%2==0){
             printf("%d",indice);
             printf("\n");
         }
@@ -95,24 +95,38 @@ scanf("%d",&num);
 #ifdef ex5
 int main(){
 
-int n=0;
+int num=0,recebe=0;
 
-printf("Digite um numero ");
-scanf("%d" ,&n);
+    while(num < 20){
 
-    for(int i;n>0;n--){
-        if(n%2==0){
-            printf("%d este numero e par",n);
-            printf("\n");
-        }else{
-            printf("%d este numero e impar",n);
-            printf("\n");
-        }
+    printf("Digite um numero: ");
+    scanf("%d",&recebe);
 
-
+    num = recebe+num;
+    printf("%d \n",num);
     }
-
 
 
 }
 #endif // ex5
+//----------------------------------------------------------------
+#ifdef ex7
+int main(){
+    int n=0;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n);
+
+    while(n >=0){
+        if(n%2==0){
+            printf("%d e par \n", n);
+
+        }else{
+            printf("%d e impar \n",n);
+
+
+        }
+        n--;
+    }
+
+}
+#endif // ex7
