22675b0daf343bc64f9a180d7252efb2727ecfca
Crituker <cristian123105@gmail.com>
2019-04-26 13:54:33 -0300

    ex 7

--- 1semestre/Nadia/vetores/vetores 28.c
+++ 1semestre/Nadia/vetores/vetores 28.c
@@ -1,10 +1,71 @@
+#include <stdio.h>
 #define ex1
-
 //exercicio 9
 #ifdef ex1
 int main(){
 
+int num = 5;
+int n =15;
 
+int a[num];
+int b[num];
+int c[num];
+int d[n];
+
+int contA=0;
+int contB=1;
+int contC=2;
+
+int negativo=0;
+
+for(int i =0 ; i<num ; i++){
+    printf("Digite um valor para o vetor a: ");
+    scanf("%d", &a[i]);
+    d[contA] = a[i];
+    contA = contA+3;
+
+    printf("Digite um valor para o vetor b: ");
+    scanf("%d", &b[i]);
+    d[contB]=b[i];
+    contB= contB+3;
+
+
+    printf("Digite um valor para o vetor c: ");
+    scanf("%d", &c[i]);
+    d[contC]=c[i];
+    contC=contC+3;
+}
+
+for(int i =0 ; i<n ; i++){
+    if(d[i]<0){
+        negativo++;
+    }
+}
+
+
+for(int i =0 ; i<num ; i++){
+
+    printf("Vetor a: %d", a[i]);
+    printf("\n");
+    printf("Vetor b: %d", b[i]);
+    printf("\n");
+    printf("Vetor c: %d", c[i]);
+    printf("\n");
+
+}
+printf("---------------------------------------------------");
+
+printf("\n");
+printf("Vetor D: ");
+printf("\n");
+
+
+for(int i =0; i< n; i++){
+    printf("%d",d[i]);
+    printf("\n");
+}
+
+printf("O vetor D teve o total de numeros negativos: %d", negativo);
 }
 #endif // ex1
 //exercicio 12
@@ -35,8 +96,8 @@ while(n >15){
 }
 
 int a[n];
-long int b[n];
-long int fat=0;
+int b[n];
+int fat=0;
 int i=0;
 
  for(int p =0 ; p < n;p++){
