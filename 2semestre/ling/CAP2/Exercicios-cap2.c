feb6542654d8ad7efdbf2dcf49d497e416be8053
Cristian <cristian123105@gmail.com>
2019-09-23 23:37:58 -0300

    ex 3 lista 2

--- 2semestre/ling/CAP2/Exercicios-cap2.c
+++ 2semestre/ling/CAP2/Exercicios-cap2.c
@@ -1,5 +1,5 @@
 #include <string.h>
-#define ex2
+#define ex3
 
 #ifdef ex1
 main(){
@@ -71,3 +71,61 @@ printf("O numero e: %d, eu sabia que ia conseguir!",media);
 }
 
 #endif // ex2
+
+#ifdef ex3
+main(){
+
+printf(" ************************** \n");
+printf(" *  Adivinhando o numero  * \n");
+printf(" ************************** \n");
+printf("\n");
+printf(" *********************************\n");
+printf(" * Pense em um numero de 0 a 9   * \n");
+printf(" * e use os comandos <(menor)    * \n");
+printf(" * =(igual) >(maior) para indicar* \n");
+printf(" * o que o numero e!             * \n");
+printf(" ********************************* \n");
+
+int media = 50;
+int maior = 99;
+int menor = 0;
+char comando ;
+
+
+do{
+
+
+    printf("-------------------------------\n");
+    printf("O seu numero e: %d ? \n",media);
+
+    comando = getchar();
+
+
+    switch(comando){
+
+        case '>':{
+            media = (maior+media)/2;
+            break;
+        }
+        case '<':{
+            maior = media;
+            media = maior/2;
+            break;
+        }
+        case '=':{
+            printf("Jogo encerrado! \n");
+            break;
+        }
+        default:{
+            printf("Opção invalida \n");
+        }
+
+    }
+
+
+}while(comando != '=');
+
+printf("O numero e: %d, eu sabia que ia conseguir!",media);
+}
+
+#endif
