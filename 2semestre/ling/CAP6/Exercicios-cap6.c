6e1f182b4c63f6556eae0c37520f82dff21193d6
Cristian <cristian123105@gmail.com>
2019-09-28 16:36:05 -0300

    Adicionando exercicios da lista 6 do 1 ao 7 completos

--- 2semestre/ling/CAP6/Exercicios-cap6.c
+++ 2semestre/ling/CAP6/Exercicios-cap6.c
@@ -1,387 +1,251 @@
 #include <stdio.h>
 #define ex8
-
-#ifdef ex1
-
-char*pesquisaLetra(char **letra[]){
-
-    char*letras[] = {"b","d","f","h","j","k","m","o","q","s","u","w","y"};
-
-    for(int i = 0; i < 13; i++){
-        if(strcmp(letra,letras[i]) == 0){
-            return "A sua letra e: %c \n",letras[i];
-        }else if(i == 12){
-            return "A sua letra nao foi entrada...";
+/*
+int rodar = 1;
+while(rodar){
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
         }
+
+*/
+#ifdef ex1
+main(){
+
+int rodar = 1;
+    while(rodar){
+        int n1,n2;
+        printf("Digite um numero: \n");
+        scanf("%d",&n1);
+        printf("Digite outro numero: \n");
+        scanf("%d",&n2);
+        int soma = n1 + n2;
+        int sub = n1 - n2;
+        int multi = n1 * n2;
+        int divi = n1 / n2;
+        printf("A soma deles %d \n",soma);
+        printf("A divisao deles %d \n",divi);
+        printf("A subtracao deles %d \n",sub);
+        printf("A multiplicao deles %d \n",multi);
+        printf("--------------------------------------------------------------\n");
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
+
     }
 
-}
-
-main(){
-char*letter[1];
-    printf("Digite uma letra: \n");
-    scanf("%s",&letter);
-
-
-    printf("%s",pesquisaLetra(letter));
 }
 #endif
 
 #ifdef ex2
-
-int soma(int a, int b){
-    return a+b;
-}
-
-int subtracao(int a, int b){
-    return a-b;
-}
-
-int multiplicao(int a, int b){
-    return a*b;
-}
-
-float divisao(int a, int b){
-    return a/b;
-}
-
 main(){
+int rodar = 1;
+    while(rodar){
+        int n1,n2;
+        printf("Digite um numero: \n");
+        scanf("%d",&n1);
+        printf("Digite outro numero: \n");
+        scanf("%d",&n2);
+        int soma = n1 += n2;
+        n1 -= n2;
+        int sub = n1 -= n2;
+        n1 +=n2;
+        int multi = n1 *= n2;
+        n1 /= n2;
+        int divi = n1 /= n2;
+        printf("A soma deles %d \n",soma);
+        printf("A divisao deles %d \n",divi);
+        printf("A subtracao deles %d \n",sub);
+        printf("A multiplicao deles %d \n",multi);
+        printf("--------------------------------------------------------------\n");
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
 
-int n1;
-int n2;
-    printf("Digite o primeiro numero: \n");
-    scanf("%d",&n1);
-
-    printf("Digite o segundo numero: \n");
-    scanf("%d",&n2);
-
-    printf("A soma desses numeros e' %d \n",soma(n1,n2));
-    printf("A subtracao desses numero e': %d \n", subtracao(n1,n2));
-    printf("A multiplicao desses numero e': %d\n",multiplicao(n1,n2));
-    printf("A divisao desses numeros e' %.2f \n",divisao(n1,n2));
+    }
 }
-#endif // ex2
+#endif
 
 #ifdef ex3
-//Variaveis globais
-int valores[];
-int i = 0;
-//Funções
-int soma(){
-    int aux = 0;
-    for(int c = 0; c <= i; c++){
-        aux = aux + valores[c];
-    }
-    return aux;
-}
-
-int subtracao(){
-    int aux = 0;
-    aux = valores[0];
-    for(int c = 1; c <= i; c++){
-        aux = aux - valores[c];
-    }
-    return aux;
-}
-
-int multiplicao(){
-    int aux = 0;
-    //aux = valores[0];
-    aux = valores[0] * valores[1];
-    for(int c = 2; c < i; c++){
-        aux = valores[c]*aux;
-    }
-    return aux;
-}
-
-float divisao(){
-    float aux = 0;
-    aux = valores[0] / valores[1];
-    for(int c = 2; c < i; c++){
-        aux = valores[c]/aux;
-    }
-    return aux;
-}
-
 main(){
-int verificacao = 1;
-
-char *letter[1];
-
-    while(verificacao){
+int rodar = 1;
+    while(rodar){
+        float n1,n2;
         printf("Digite um numero: \n");
-        scanf("%d",&valores[i]);
-        i++;
-        printf("Voce deseja inserir um novo valor? s/n \n");
-        scanf("%s",&letter);
-
-        if(strcmp(letter,"s") == 0){
-            continue;
-        }else{
-            break;
-        }
+        scanf("%f",&n1);
+        printf("Digite outro numero: \n");
+        scanf("%f",&n2);
+        float soma = n1 += n2;
+        n1 -= n2;
+        float sub = n1 -= n2;
+        n1 +=n2;
+        float multi = n1 *= n2;
+        n1 /= n2;
+        float divi = n1 /= n2;
+        printf("A soma deles %.2f \n",soma);
+        printf("A divisao deles %.2f \n",divi);
+        printf("A subtracao deles %.2f \n",sub);
+        printf("A multiplicao deles %.2f \n",multi);
+        printf("--------------------------------------------------------------\n");
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
 
     }
-
-    printf("A soma desses numeros e' %d \n",soma());
-    printf("A subtracao desses numero e': %d \n", subtracao());
-    printf("A multiplicao desses numero e': %d \n",multiplicao());
-    printf("A divisao desses numeros e' %.2f \n",divisao());
 }
 #endif
 
 #ifdef ex4
-//Variaveis globais
-int valores[];
-int i = 0;
-//Funções
-int soma(){
-    int aux = 0;
-    for(int c = 0; c <= i; c++){
-        aux = aux + valores[c];
-    }
-    return aux;
-}
-
-int subtracao(){
-    int aux = 0;
-    aux = valores[0];
-    for(int c = 1; c <= i; c++){
-        aux = aux - valores[c];
-    }
-    return aux;
-}
-
-int multiplicao(){
-    int aux = 0;
-    //aux = valores[0];
-    aux = valores[0] * valores[1];
-    for(int c = 2; c < i; c++){
-        aux = valores[c]*aux;
-    }
-    return aux;
-}
-
-float divisao(){
-    float aux = 0;
-    aux = valores[0] / valores[1];
-    for(int c = 2; c < i; c++){
-        aux = valores[c]/aux;
-    }
-    return aux;
-}
-
 main(){
-int verificacao = 1;
-
-char *letter[1];
-
-    while(verificacao){
+int rodar = 1;
+while(rodar){
+        int n1,n2;
         printf("Digite um numero: \n");
-        scanf("%d",&valores[i]);
-        i++;
-        printf("Voce deseja inserir um novo valor? s/n \n");
-        scanf("%s",&letter);
+        scanf("%d",&n1);
+        printf("Digite outro numero: \n");
+        scanf("%d",&n2);
 
-        if(strcmp(letter,"s") == 0){
-            continue;
-        }else{
-            break;
-        }
+        printf("Operadores bit-a-bit\n");
+        printf("AND  n1 & n2 --> %3d  0x%x\n",n1 & n1,n1 & n2);
+        printf("OU   n1 | n2 --> %3d  0x%x\n",n1 | n2,n1 | n2);
+        printf("OU EXCLUSIVO   n1 ^ n2 --> %3d  0x%x\n",n1^ n2,n1 ^ n2);
 
-    }
-    char operacao;
-    printf("Qual operacao voce deseja realizar com esses numeros? Digite o operador aritmetico: \n");
-    scanf("%s",&operacao);
-    int ascii = (int) operacao;
-    printf("ascii = %d\n",ascii);
-    switch(ascii){
-        case (42):{
-            printf("A multiplicao desses numero e': %d \n",multiplicao());
-            break;
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
         }
-        case(43):{
-            printf("A soma desses numeros e' %d \n",soma());
-            break;
-        }
-        case(45):{
-            printf("A subtracao desses numero e': %d \n", subtracao());
-            break;
-        }
-        case(47):{
-            printf("A divisao desses numeros e' %.2f \n",divisao());
-            break;
-        }
-        default:{
-            printf("Digite um operador valido \n");
-        }
-}
-
-
-
-
 }
 #endif
 
 #ifdef ex5
-int comparaString(char str[10], char str2[10]){
-    int countStr = 0;
-    int countStr2 = 0;
-    if(strcmp(str,str2)==0){
-        return 1;
-    }else{
-        for(int i = 0; i < 1000; i++){
-            if(str[i]){
-                countStr++;
-            }else{
-                break;
-            }
-        }
-        for(int i = 0; i < 1000; i++){
-            if(str2[i]){
-                countStr2++;
-            }else{
-                break;
-            }
-        }
-        if(str > str2){
-            return 2;
-        }else{
-            return 3;
-        }
-    }
-}
-
 main(){
-    char str[10];
-    char str2[10];
-    printf("Digite a primeira string: \n");
-    scanf("%s",&str);
+int rodar = 1;
+int n;
+while(rodar){
+        printf("Digite um numero: ");
+        scanf("%d",&n);
+        int mult = n << 3;
+        int div = n >> 3;
+        printf("Multiplicacao = %d e Divisao = %d \n",mult,div);
 
-    printf("Digite a segunda string: \n");
-    scanf("%s",&str2);
-    int result = comparaString(str,str2);
-    printf("O retorno da funcao e: %d",result);
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
+        }
 }
 #endif
 
 #ifdef ex6
-int somaFatorial(int num){
-    int fat;
-    for(fat = 1; num> 1; num = num-1 ){
-        fat = fat*num;
-    }
-    return fat;
-}
-
 main(){
-int cont =1;
-int decimal,unidade,n;
-printf("Digite um numero: \n");
-scanf("%d",&n);
-
-int result = somaFatorial(n);
-
-decimal = result;
-if(decimal > 10){
-    cont++;
-    decimal = decimal/10;
+int rodar = 1;
+while(rodar){
+int a,b;
+        printf("Digite um valor para a: \n");
+        scanf("%d",&a);
+        printf("Digite um valor para b: \n");
+        scanf("%d",&b);
+        if(a >= 120 && b < 240 ){
+            printf("verdadeiro: %d \n",1);
+        }else{
+            printf("falso: %d \n",0);
+        }
+printf("------------------------------------------------\n");
+        printf("Digite um valor para a: \n");
+        scanf("%d",&a);
+        printf("Digite um valor para b: \n");
+        scanf("%d",&b);
+        if(a != 60  || b == 120){
+            printf("verdadeiro: %d \n",1);
+        }else{
+            printf("falso: %d \n",0);
+        }
+        printf("------------------------------------------------\n");
+        printf("Digite um valor para a: \n");
+        scanf("%d",&a);
+        printf("Digite um valor para b: \n");
+        scanf("%d",&b);
+        if((a*25) == 100 && (b+10) >= 100){
+            printf("verdadeiro: %d \n",1);
+        }else{
+            printf("falso: %d \n",0);
+        }
+        printf("------------------------------------------------\n");
+        printf("Digite um valor para a: \n");
+        scanf("%d",&a);
+        printf("Digite um valor para b: \n");
+        scanf("%d",&b);
+        if(((a + 300)/5) >= 100 || (b - 200) <= 200){
+            printf("verdadeiro: %d \n",1);
+        }else{
+            printf("falso: %d \n",0);
+        }
+        printf("------------------------------------------------\n");
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
+        }
 }
-    unidade = result-(cont*10);
-
-int soma = unidade + decimal;
-printf("O resultado de %d + %d = %d \n",decimal,unidade,soma);
-}
-#endif // ex6
+#endif
 
 #ifdef ex7
-int exponenciacao(int numero,int expoente){
-    int result = numero;
-
-    for(int i = 0; i < expoente;i++){
-        result = result*numero;
-    }
-    return result;
-}
 main(){
-int resultado;
-int num,expo;
-printf("Digite para a base \n");
-scanf("%d",&num);
+int rodar = 1;
+float celsius, kelvin,fahr;
+    while(rodar){
+        printf("Digite um valor em Celsius: \n");
+        scanf("%f",&celsius);
+        float celsiusKelvin = celsius+273;
+        float celsiusFahr = (celsius*1.8)+32;
+        printf("Celsius em Kelvin: %.2f e Farhrenheit %.2f \n",celsiusKelvin,celsiusFahr);
+        printf("--------------------------------------------\n");
+        printf("Digite um valor em Fahrenheit: \n");
+        scanf("%f",&fahr);
+        float fahrCelsius = (fahr-32)/1.8;
+        float fahrKelvin = ((fahr-32)/1.8)+273.15;
+        printf("Farhrenheit em Kelvin: %.2f e Celsius %.2f \n",fahrKelvin,fahrCelsius);
+        printf("--------------------------------------------\n");
+        printf("Digite um valor em Kelvin: \n");
+        scanf("%f",&kelvin);
 
-printf("Digite um numero para o expoente \n");
-scanf("%d",&expo);
 
-resultado = exponenciacao(num,expo);
-printf("%d",resultado);
+        float kelvinCelsius = kelvin-273;
+        float kelvinFahr =(kelvin - 273.15)* 1.8000+ 32.00;
+
+        printf("Kelvin em Farhrenheit: %.2f e Celsius %.2f \n",kelvinFahr,kelvinCelsius);
+        printf("--------------------------------------------\n");
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0. \n");
+        scanf("%d",&rodar);
+    }
 }
-#endif // ex7
+#endif
 
 #ifdef ex8
-int sinal(int num){
-    if(num < 0){
-        return 0;
-    }
-    if(num >0){
-        return 1;
-    }
-    if(num == 0){
-        return -1;
-    }
-}
-
-int soma(int n1, int n2){
-    int comeco,fim;
-    int tot=0;
-    if(n1 > n2){
-        comeco = n2;
-        fim = n1;
-    }else{
-        comeco = n1;
-        fim = n2;
-    }
-    //tot = comeco;
-for(int i = comeco; i < fim; i++ ){
-    tot = tot + i;
-}
-return tot;
-}
-
-int multi(int n1, int n2){
-    int comeco,fim;
-    int tot=0;
-    if(n1 > n2){
-        comeco = n2;
-        fim = n1;
-    }else{
-        comeco = n1;
-        fim = n2;
-    }
-    //tot = comeco;
-for(int i = comeco; i < fim; i++ ){
-    tot = tot * i;
-}
-}
 main(){
-int n1,n2;
-printf("Digite um numero: \n");
-scanf("%d",&n1);
+int rodar = 1;
+int anoNasc,mesNasc,diaNasc,anoAtual,mesAtual,diaAtual;
+    while(rodar){
+    //Coletando data de nascimento
+        printf("Digite o ano em que voce nasceu: \n");
+        scanf("%d",&anoNasc);
 
-int s1 = sinal(n1);
+        printf("Digite o mes em que voce nasceu: \n");
+        scanf("%d",&mesNasc);
 
-printf("Digite outro numero :\n");
-scanf("%d",&n2);
+        printf("Digite o dia em que voce nasceu: \n");
+        scanf("%d",&diaNasc);
 
-int s2 = sinal(n2);
-int s;
+        printf("--------------------------------------------\n");
+    //Coletando data atual
+        printf("Digite o ano atual: \n");
+        scanf("%d",&anoAtual);
 
-if(s1 == s2 == 1){
-    printf("A soma dos numero e %d", soma(n1,n2));
+        printf("Digite o mes atual: \n");
+        scanf("%d",&mesAtual);
+
+        printf("Digite o dia atual: \n");
+        scanf("%d",&diaAtual);
+        printf("--------------------------------------------\n");
+    //Trabalhando com os dados
+
+
+
+
+        printf("--------------------------------------------\n");
+        printf("Caso queira rodar novamente digite 1 se quiser parar digite 0 \n");
+        scanf("%d",&rodar);
+    }
 }
-if(s1 ==s2 == 0){
-    printf("A multiplicao entre esses numero e %d", multi(n1,n2));
-}
-
-soma(n1,n2);
-}
-#endif // ex8
+#endif
