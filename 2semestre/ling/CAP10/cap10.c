f603617a0bb2d497765b25bdfa0f5f3ec8c7faac
Cristian <cristian123105@gmail.com>
2019-11-05 14:28:54 -0300

    cap10 ex1

--- 2semestre/ling/CAP10/cap10.c
+++ 2semestre/ling/CAP10/cap10.c
@@ -1,3 +1,49 @@
 #include "stdio.h"
-#define ex4
+#include "stdlib.h"
 
+#define ex1
+
+#ifdef ex1
+main(){
+FILE *p;
+char ch;
+char a[100];
+int i = 0;
+int x = 0;
+
+
+    if((p = fopen("arq.txt","w")) == NULL){
+        printf("Erro ao abrir o arquivo. \n");
+        exit(0);
+    }
+
+    for(;;){
+        printf("------------------------------------\n");
+        printf("Digite um caractere para inserior no arquivo\nDigite 0 para sair\n");
+        scanf("%c",&ch);
+        getchar();
+
+        putc(ch,p);
+
+        if(ch == '0'){
+            break;
+            fclose(p);
+        }
+        i++;
+    }
+    printf("Exibindo conteudo escrito no arquivo\n");
+    if((p = fopen("arq.txt","r")) == NULL){
+        printf("Erro ao abrir o arquivo. \n");
+        exit(0);
+    }
+fgets(a,100,p);
+printf("Exibindo strings do arquivo \n");
+
+
+fputs(a,p);
+
+fclose(p);
+
+
+}
+#endif // ex1
